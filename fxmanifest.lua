8c9f7c7317d936fd52d7e3f0872384f12759f063
AnonRobAta <79940910+AnonRobAta@users.noreply.github.com>
2022-01-29 01:21:52 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+game 'gta5'
+
+fx_version 'cerulean'
+
+server_scripts {
+   'server.lua'
+}
+
+server_exports {
+   "CheckPlayerTrigger",
+   "GetSecurityTrigger",
+   "RegenerateNewPlayersDataTrigger",
+}
