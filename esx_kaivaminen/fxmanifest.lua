699c20dcc5be5e215e4bc35eb50fd7268eba9334
AnonRobAta <79940910+AnonRobAta@users.noreply.github.com>
2022-02-01 17:54:28 +0200

    Example filu josta ottaa mallia!

---
+++ esx_kaivaminen/fxmanifest.lua
@@ -0,0 +1,17 @@
+game 'gta5'
+
+fx_version 'cerulean'
+
+author 'roba'
+
+client_scripts {
+    'client.lua',
+    'config.lua'
+}
+
+shared_script 'config.lua'
+
+server_scripts {
+    'server.lua',
+    's_config.lua'
+}
